d17746f35a3492541d9a486adbf12c694980c35a
gatilov2022 <99524673+gatilov2022@users.noreply.github.com>
2023-03-07 09:39:52 +0500

    Create  class FastSort and TreeNode. (#4)
    Co-authored-by: Сергей Гатилов <102010026@ad.kgsu.ru>

---
+++ Practice/FastSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class FastSort
+    {
+        private static void Swap(ref char left, ref char right)
+        {
+            var t = left;
+            left = right;
+            right = t;
+        }
+
+        //метод возвращающий индекс опорного элемента
+        private static int Partition(char[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        //быстрая сортировка
+        private static char[] QuickSort(char[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        public static string QuickSort(char[] array)
+        {
+            return new string(QuickSort(array, 0, array.Length - 1));
+        }
+    }
+}
---
+++ Practice/TreeNode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class TreeNode
+    {
+        //Переменная для символов
+        private char Data { get; set; }
+
+        //Левый узел дерева
+        private TreeNode Left { get; set; }
+
+        //Правый узел дерева
+        private TreeNode Right { get; set; }
+        //Сортировка всей строки
+        public static string TreeSort(char[] array)
+        {
+            var treeNode = new TreeNode(array[0]);
+            for (int i = 1; i < array.Length; i++)
+            {
+                treeNode.Insert(new TreeNode(array[i]));
+            }
+
+            return new string(treeNode.Transform());
+        }
+        //Добавление в переменную символом
+        private TreeNode(char data)
+        {
+            Data = data;
+        }
+
+        //Добавление символов в узлы дерева
+        private void Insert(TreeNode node)
+        {
+            if (node.Data < Data)
+            {
+                if (Left == null) Left = node;
+                else Left.Insert(node);
+            }
+            else
+            {
+                if (Right == null) Right = node;
+                else Right.Insert(node);
+            }
+        }
+
+        //преобразование дерева в отсортированный массив
+        private char[] Transform(List<char> elements = null)
+        {
+            if (elements == null) elements = new List<char>();
+
+            if (Left != null) Left.Transform(elements);
+
+            elements.Add(Data);
+
+            if (Right != null) Right.Transform(elements);
+
+            return elements.ToArray();
+        }
+    }
+}
